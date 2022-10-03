64d2e91642b9607863f7ca48e3b3af8fbfef89eb
Nguyen Dang Trung <trungndph29562@fpt.edu.vn>
2022-10-03 19:04:42 +0700

    fix upload image category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,6 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = [
-        'name','slug','avatar','status', 'parent_id'
+        'name','slug','avatar','status', 'parent_id','image'
     ];
 }
