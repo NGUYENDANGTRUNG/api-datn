cfbfb409d57d1d1e963357fd9b8ab63d6b7d6cac
Nguyen Dang Trung <trungndph29562@fpt.edu.vn>
2022-10-12 00:47:32 +0700

    create assets for feature checkout

---
+++ database/migrations/2022_10_11_171756_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id');
+            $table->string('total', 100)->nullable();
+            $table->tinyText(' note');
+            $table->string('vnp_response_code',255);
+            $table->string('code_vnp',255);
+            $table->string('code_bank',255);
+            $table->dateTime('pay_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
